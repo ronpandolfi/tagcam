e63e581fb5a9a8aa6e9d39a4d633f18bfa15ef17
Ronald Pandolfi <ronpandolfi@gmail.com>
2018-07-25 14:40:22 -0700

    fix metaclassing

--- tagcam/user/forms.py
+++ tagcam/user/forms.py
@@ -126,7 +126,8 @@ class TomoTagForm(FlaskForm):
         # get the group of files
         tomodatafiles = session.query(TomoDataFile).filter(
             DataFile.tagged < 2).filter(TomoDataFile.groupid==tomodatafile.groupid)
-        attrs = dict(cls().__dict__)
+        attrs = dict(FlaskForm.__dict__)
+        attrs.update(dict(cls.__dict__))
         attrs['groupcount'] = tomodatafiles.count
         attrs['qualityradios'] = []
 
